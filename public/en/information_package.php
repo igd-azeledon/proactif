d8430939b0cdfdbf679cd361074f6215d5eae4a9
jphernandez <jphernandez@intergraphicdesigns.com>
2013-06-18 16:35:53 -0600

    add new secondary pages with english content

---
+++ public/en/information_package.php
@@ -0,0 +1,94 @@
+<!doctype html>
+<html class="no-js" lang="fr">
+<head>
+
+<meta charset="utf-8">
+
+<title>PROACTIF - INFORMATION PACKAGE</title>
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+
+<link rel="shortcut icon" href="../favicon.ico" />
+
+<!--CSS STARTS-->
+<link rel="stylesheet" type="text/css" href="../css/default.css" />
+<link rel="stylesheet" type="text/css" href="../css/page.css" />
+<!--CSS ENDS-->
+
+<script src="../js/libs/modernizr-2.0.min.js"></script>
+
+</head>
+<body>
+
+<!--TOP STARTS-->
+<?php include_once("../includes/_include_top_en.php"); ?>
+<!--TOP ENDS-->
+
+<section class="banner pic5"><div><h2 class="different">Information package<span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis erat at dolor commodo eleifend. Vestibulum cursus</span></h2></div></section>
+
+<!--PAGE CONTENT STARTS-->
+<section id="PageContent">
+
+<!--CENTER CONTENT STARTS-->
+	<section id="CenterContent">
+		<ul class="links">
+			<li><a href="../index.php" title="Home">Home</a>></li>
+			<li>Information package</li>
+		</ul>
+		<p><em>Get your Information kit online</em></p>
+		<h1>Your satisfaction is guaranteed!</h1>
+		<article>
+			<p>Your protection begins the minute we receive your <a href="#" title="Enrollment Form">completed Enrollment Form</a>. The first premium payment will be withdrawn from your account 10 days after your enrolment.</p>
+			<p>If for any reason, you are not fully satisfied, simply return your policy to us within 30 days and you will receive a full refund of all the premiums paid, with <strong>no obligation</strong> on your part.</p>
+			<p>For more information or filling out an <a href="#" title="Enrollment Form">Enrollment Form</a>, please refer to the following documents.</p>
+			<ul id="Documents">
+				<li><a href="#" title="ProActive, Personal Injury Insurance Brochure"><strong>ProActive, Personal Injury Insurance Brochure (<img src="../images/page/icon_acrobat.gif" alt="Acrobat document" />)</strong></a></li>
+				<li><a href="#" title="Medical Care Assistance Brochure"><strong>Medical Care Assistance Brochure (<img src="../images/page/icon_acrobat.gif" alt="Acrobat document" />)</strong></a></li>
+				<li><a href="#" title="ProActive, Personal Injury Insurance Enrollment form"><strong>ProActive, Personal Injury Insurance Enrollment form (<img src="../images/page/icon_acrobat.gif" alt="Acrobat document" />)</strong></a></li>
+<!--				<li><a href="#" title="Formulaire de réclamation ProActif, Assurance en cas de blessure">Formulaire de réclamation <strong>ProActif, Assurance en cas de blessure (<img src="images/page/icon_acrobat.gif" alt="Acrobat document" />)</a></strong></li>-->
+			</ul>
+			<p class="joindre">Would you rather speak to us? <a href="../contact-us.php" class="more">Contact us!</a></p>
+		</article>
+	</section>
+<!--CENTER CONTENT ENDS-->
+
+<!--ASIDE STARTS-->
+	<aside>
+	  <img src="../images/page/image_01.jpg" alt="" />
+  </aside>
+<!--ASIDE ENDS-->
+
+  <div class="clearfix"></div>
+</section>
+<!--PAGE CONTENT ENDS-->
+
+<!--FOOTER STARTS-->
+<?php include_once("../includes/_include_bottom_en.php"); ?>
+<!--FOOTER ENDS-->
+
+<!--FOOTER STARTS-->
+<?php include_once("../includes/_include_footer_en.php"); ?>
+<!--FOOTER ENDS-->
+
+<!--SCRIPS START-->
+<!--JavaScript at the bottom for fast page loading-->
+
+<!--Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline-->
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/jquery-1.8.2.min.js"><\/script>')</script>
+
+<script type="text/javascript" src="../js/jquery.tools.min.js"></script>
+
+<!--scripts concatenated and minified via ant build script-->
+<script defer src="../js/script.js"></script><!--Para colocar inizializadores de JS-->
+<!--end scripts-->
+
+<!--SCRIPS ENDS-->
+
+<!--[if lt IE 7 ]>
+	<script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.2/CFInstall.min.js"></script>
+	<script>window.attachEvent("onload",function(){CFInstall.check({mode:"overlay"})})</script>
+<![endif]-->
+
+</body>
+</html>
