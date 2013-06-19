ec1c8a487ee02d9758abf200c784d604545507d0
jphernandez <jphernandez@intergraphicdesigns.com>
2013-06-19 08:58:08 -0600

    new english content

---
+++ public/en/conditions-use.php
@@ -0,0 +1,97 @@
+<!doctype html>
+<html class="no-js" lang="fr">
+<head>
+
+<meta charset="utf-8">
+
+<title>PROACTIF - CONDITIONS OF USE</title>
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+
+<link rel="shortcut icon" href="../favicon.ico" />
+
+<!--CSS STARTS-->
+<link rel="stylesheet" type="text/css" href="../css/default.css" />
+<link rel="stylesheet" type="text/css" href="../css/page.css" />
+<!--CSS ENDS-->
+
+<script src="../js/libs/modernizr-2.0.min.js"></script>
+
+</head>
+<body>
+
+<!--TOP STARTS-->
+<?php include_once("../includes/_include_top_en.php"); ?>
+<!--TOP ENDS-->
+
+<!--PAGE CONTENT STARTS-->
+<section id="PageContent">
+
+<!--CENTER CONTENT STARTS-->
+	<section id="CenterContent">
+		<h1>Conditions of Use</h1>
+		<h2>IMPORTANT NOTICE TO ALL USERS OF PROACTIVE, PERSONAL INJURY INSURANCE WEBSITE OF NATIONAL BANK INSURANCE</h2>
+		<p>When using the <strong>ProActive, Personal Injury Insurance</strong> website (the “website”), you accept the terms of use described herein, which are subject to change without notice. Furthermore, you agree to consult these conditions of use from time to time and acknowledge that continuing to use the website means you have accepted any changes that could have been made.</p>
+		<h3>Governing legislation</h3>
+		<p>This website will be governed by and construed in accordance with the laws of Canada and the applicable laws of the Province of Quebec, without giving effect to any principles of conflicts of laws. Any litigation, dispute or claim arising from or relative to the use of this website shall be subject to the jurisdiction of the courts of Quebec and you accept henceforth to be submitted to the exclusive jurisdiction of the courts of Quebec in this respect.</p>
+		<h3>Responsibility of the user</h3>
+		<p>Laws in effect and the National Bank Insurance security standards, which are described under ABCs of Security.</p>
+		<h3>Accessing and using the website</h3>
+		<p>National Bank Insurance cannot guarantee that the site will be accessible at all times, without interruption, and that it will be free of errors. National Bank Insurance cannot be held liable towards you or anyone for any damages that could result from the inaccessibility of the website or from any difficulties you might experience accessing or using the website due to, in particular, an interruption to the service, a network failure, the malfunction of any computer hardware or software, any faulty, incomplete, incomprehensible or deleted transmission, brought to its attention or not.</p>
+		<p>National Bank Insurance reserves the right to suspend at any time, without warning, full or partial access to the www.nbcinjuryinsurance.ca website or to stop uploading information. In such case, you agree to indemnify and hold NBC Assistance Inc. harmless from any liability related to damages that could result from such an initiative.</p>
+		<h3>Contents</h3>
+		<p>The contents of the website are provided for information purposes only, unless otherwise stated. This content does not constitute an offer to purchase or sell National Bank Insurance’s products or services or solicitation on behalf authorized third parties.</p>
+		<p>The contents of this website must not be interpreted, considered or used as if it were financial, legal, fiscal, or other advice. National Bank Insurance will not be liable for any damages that you may incur from such use.</p>
+		<p>The information contained in this website is the property of National Bank Insurance and may not be reproduced, in whole or in part, unless for your personal use. In all other cases, you must obtain prior consent from National Bank Insurance before reproducing any contents of this site.</p>
+		<h3>Confidentiality and security</h3>
+		<p>National Bank Insurance complies with the legislation governing the protection of personal information. In addition, it uses the latest encryption techniques to guarantee the confidentiality of the information. National Bank Insurance, however, cannot protect information exchanged when the website is accessed via a non-secure platform and/or in a public location, which could endanger the confidentiality of your personal information. National Bank Insurance cannot be held liable for any damages you may incur under similar circumstances. Please consult the ABCs of Security for more information on this topic.</p>
+		<p>Communications via e-mail are not secure or confidential. They can be lost or intercepted, in much the same way as communications via other means (for example, cellular phone, post office mail). National Bank Insurance cannot be held liable for any damages that you may incur from any exchange of e-mail or electronic messages, between you and National Bank Insurance.</p>
+		<p>In general, you alone are responsible for ensuring the protection and safekeeping of your data and computer platforms/devices.</p>
+		<h3>Accuracy and changes in information on our website</h3>
+		<p>Even if National Bank Insurance does not guarantee that the information is exact, compliant or complete, the data and information contained on the website that comes from National Bank Insurance and other sources are believed to be reliable when posted. Despite its best efforts, however, National Bank Insurance cannot guarantee that the information is exact or complete, free from errors or omissions or that it is up to date at all times.</p>
+		<p>The data and information available on this site may be changed, moved or deleted without notice.</p>
+		<h3>Limitation of liability</h3>
+		<p>In general and without limiting the specific cases of limitation of liability mentioned above, National Bank Insurance cannot, notwithstanding any provisions to the contrary, be held liable by you or anyone else for any damages whatsoever that may result from or be related to the site and/or its contents, particularly, without limiting the generality of the foregoing, to their access, their use, the inaccessibility of the website, their unusable nature, the inaccuracy of its contents or the loss of data or information.</p>
+		<h3>Miscellaneous</h3>
+		<p>Should any paragraph of these conditions be declared or ruled as illegal, unenforceable or null and void by a court of competent jurisdiction, the other paragraphs will remain valid and will continue to apply within the limits permitted by law.</p>
+	</section>
+<!--CENTER CONTENT ENDS-->
+
+<!--ASIDE STARTS-->
+	<?php include_once("../includes/_include_aside_en.php"); ?>
+<!--ASIDE ENDS-->
+
+  <div class="clearfix"></div>
+</section>
+<!--PAGE CONTENT ENDS-->
+
+<!--FOOTER STARTS-->
+<?php include_once("../includes/_include_bottom_en.php"); ?>
+<!--FOOTER ENDS-->
+
+<!--FOOTER STARTS-->
+<?php include_once("../includes/_include_footer_en.php"); ?>
+<!--FOOTER ENDS-->
+
+<!--SCRIPS START-->
+<!--JavaScript at the bottom for fast page loading-->
+
+<!--Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline-->
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/jquery-1.8.2.min.js"><\/script>')</script>
+
+<script type="text/javascript" src="../js/jquery.tools.min.js"></script>
+
+<!--scripts concatenated and minified via ant build script-->
+<script defer src="../js/script.js"></script><!--Para colocar inizializadores de JS-->
+<!--end scripts-->
+
+<!--SCRIPS ENDS-->
+
+<!--[if lt IE 7 ]>
+	<script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.2/CFInstall.min.js"></script>
+	<script>window.attachEvent("onload",function(){CFInstall.check({mode:"overlay"})})</script>
+<![endif]-->
+
+</body>
+</html>
